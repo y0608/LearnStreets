fa735a4b35cef786fc2f9051f6d429a0fa0e16f5
Yoan Laptop <yoanivanov9@gmail.com>
2023-02-08 16:37:00 +0200

    dash

---
+++ testing/dash/bubbles.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+df = px.data.gapminder()
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     projection="natural earth")
+fig.show()
--- testing/dash/sofia.py
+++ testing/dash/sofia.py
@@ -5,8 +5,7 @@ geojson = px.data.election_geojson()
 
 fig = px.choropleth_mapbox(df, geojson=geojson, color="winner",
                            locations="district", featureidkey="properties.district",
-                           center={"lat": 42.698334, "lon": 23.319941},
-                           mapbox_style="carto-positron", zoom=12)
+                           center={"lat": 42.698334, "lon": 23.319941}, zoom=12)
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
